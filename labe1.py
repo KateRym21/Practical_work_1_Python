eae15634fb84e9b929b4d3e142b39649332e9f1a
Kateryna Rymarchuk <143935308+KateRym21@users.noreply.github.com>
2026-09-10 22:10:48 +0300

    Add files via upload

---
+++ labe1.py
@@ -0,0 +1,50 @@
+print('Hello, world!')
+
+name = "Katya"
+age = 17
+height = 1.65
+is_student = True
+
+
+my_list = ["Python", "IT", "English"]
+my_tuple = (10, 20, 30)
+my_dict = {"name": "Katya", "age": 17}
+my_set = {1,2,3,4}
+
+
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(is_student, type(is_student))
+print(my_list, type(my_list))
+print(my_tuple, type(my_tuple))
+print(my_dict, type(my_dict))
+print(my_set, type(my_set))
+
+
+
+a = 10
+b = 3
+
+print("Додавання:", a + b)
+print("Віднімання:", a - b)
+print("Множення:", a * b)
+print("Ділення:", a / b)
+print("Цілочисельне ділення:", a // b)
+print("Остача від ділення:", a % b)
+print("Піднесення до степеня:", a ** b)
+
+
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+
+first_name = "Katya"
+last_name = "Student"
+
+print(first_name + " " + last_name)
